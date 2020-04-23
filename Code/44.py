95290e220fd783544cb503fd9a9672314bc7344a
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2020-04-23 20:46:18 +0800

    提交代码

---
+++ Code/44.py
@@ -0,0 +1,18 @@
+#coding= utf-8
+print('已成功拉取Git仓库中的代码，开始执行代码')
+print(1)
+print(2)
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('blink-settings=imagesEnabled=false')
+chrome_options.add_argument('--disable-gpu')
+browser = webdriver.Chrome(chrome_options=chrome_options)
+browser.get('http://www.baidu.com')
+title=browser.title
+print('当前标题是：'+title)
+print('OK')
+print('执行完毕')
