979e9da573c8a21853415bd26765f65c4ef36760
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2020-12-04 20:37:42 +0800

    add:SudentManagement

---
+++ Study Java/StudentsManage/studentManeger/Students.java
@@ -0,0 +1,52 @@
+package studentManeger;
+
+public class Students {
+    //学生ID
+    private String sid;
+    //学生姓名
+    private String name;
+    //学生年龄
+    private int age;
+    //学生地址
+    private String address;
+    public Students(){
+
+    }
+    public Students(String sid,String name,int age,String address){
+        this.sid = sid;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    public String getSid(){
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
