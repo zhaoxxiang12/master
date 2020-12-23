c1dad400692b83bf9801a612940a1f3428a67b92
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2020-12-23 19:25:34 +0800

    update:Java 面向对象 封装加强

---
+++ Study Java/JavaBased/HomeworkDemo/StringDemo.java
@@ -0,0 +1,19 @@
+package HomeworkDemo;
+
+import java.util.Arrays;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        /**
+         * str = "I love you" 变为 you love I
+         */
+        String str = "I love you";
+        String[] split = str.split(" ");
+        System.out.println(Arrays.toString(split));
+        for(int i =split.length-1;i>=0;i--){
+            System.out.println(split[i]+" ");
+        }
+        // 字符串的分割 split(regx)
+
+    }
+}
