78608be70a184f9aec97bfbabda4b534cec41bbc
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-03-05 19:26:56 +0800

    update:更新Java代码

--- Study Java/API_JAVA/LinkedList/LinkedDemo.java
+++ Study Java/API_JAVA/LinkedList/LinkedDemo.java
@@ -2,6 +2,9 @@ package LinkedList;
 
 import java.util.LinkedList;
 
+/**
+ * 使用场景  出频繁添加，删除操作时建议使用LinkedList
+ */
 public class LinkedDemo {
     public static void main(String[] args) {
         // LinkedList通过Deque(双端队列)来实现的(双向链表)
@@ -14,10 +17,18 @@ public class LinkedDemo {
         list.add(5);
         list.addFirst(666);// 从链表头开始添加元素
         list.addLast(888); // 从链表尾开始添加元素
+        list.removeFirst(); // 从表头删除
+        list.removeLast(); //从表尾删除
 
         //2.删除
         list.remove();//默认从表头开始删除
-        list.remove(3); //删除下标3的元素
+        list.remove(3); //删除下标3的元素  删除原理：先查询到然后再删除
         System.out.println(list);
+
+        // 常用方法
+        list.clear(); //清空集合
+        System.out.println(list.size());//求集合长度
+        System.out.println(list.contains(3));//判断是否包含某个元素
+        list.
     }
 }
