29655b6cd6d3c59eac4d49183ce91d9b02410771
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-12 19:38:51 +0800

    提交线程安全学习文件

--- Study Java/API_JAVA/ThredSecurity/NumberIncrement.java
+++ Study Java/API_JAVA/ThredSecurity/NumberIncrement.java
@@ -1,15 +1,22 @@
 package ThredSecurity;
 
+/**
+ * 表示获取字节码对象NumberIncrement.class
+ * 字节码对象是唯一的(当锁使用)
+ * 口诀：无论什么方法或者代码块，使用字节码当锁对象一定是正确的
+ */
 public class NumberIncrement {
     private static int i = 0;
-
-    public  void increment() {
-        ++i;
-        System.out.println(Thread.currentThread().getName()+i);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+//    public static final Object obj = new Object(); //锁对象一定要唯一
+    public void increment() {
+        synchronized (Test.class) { //this 需要慎重使用(与苦难导致锁不唯一)
+            ++i;
+            System.out.println(Thread.currentThread().getName() + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
