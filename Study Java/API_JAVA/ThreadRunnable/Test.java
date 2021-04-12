41177c89ac53e0db7089afa2984ad292a9131ae1
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-12 11:17:01 +0800

    线程创建第二种方式

--- Study Java/API_JAVA/ThreadRunnable/Test.java
+++ Study Java/API_JAVA/ThreadRunnable/Test.java
@@ -3,9 +3,18 @@ package ThreadRunnable;
 public class Test {
     public static void main(String[] args) {
         //1.创建一个
-        MyRunnable target = new MyRunnable();
-        Thread thread = new Thread();
+        Runnable target = new MyRunnable();
+        Thread thread = new Thread(target);
         thread.start();
 
+        //2.使用匿名内部类来创建接口类型的对象
+      Runnable runnable =   new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("you are welcome");
+            }
+        };
+        Thread t2 = new Thread(runnable);
+        t2.start();
     }
 }
