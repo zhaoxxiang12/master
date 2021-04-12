490c207323745169bd9ed9b43840cec73b10fcfc
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-12 11:15:28 +0800

    文件增加注释

--- Study Java/API_JAVA/Thread/Printer.java
+++ Study Java/API_JAVA/Thread/Printer.java
@@ -1,11 +1,16 @@
 package Thread;
 
+/**
+ * 打印机
+ *   线程不安全现象(指令重排导致)
+ *   【同步】:保留整体
+ */
 public class Printer {
     public void printA(){
         System.out.print("A");
         System.out.print("A");
         System.out.print("A");
-        System.out.print("A");
+        System.out.print("A"); //会出现指令重排现象
         System.out.println("A");
 
     }
