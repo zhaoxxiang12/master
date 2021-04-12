6e4f226a47462b01bb197921e7e35be7c891a392
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-12 10:56:58 +0800

    提交线程注释

--- Study Java/API_JAVA/Thread/Test3.java
+++ Study Java/API_JAVA/Thread/Test3.java
@@ -1,5 +1,15 @@
 package Thread;
 
+/***
+ * 线程的第一种创建方式 Thread
+ * class extends Thread
+ * new Thread(){
+ *     匿名内部类方式
+ *     run(){
+ *         
+ *     }
+ * }
+ */
 public class Test3 {
     public static void main(String[] args) {
         //创建线程，一个打印A,一个打印B
