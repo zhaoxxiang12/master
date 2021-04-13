8698f73387fe04afdce2ed263b6b8c71bfde70b6
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 10:44:10 +0800

    update:线程作业自增数

--- Study Java/API_JAVA/ThreadWork/DB.java
+++ Study Java/API_JAVA/ThreadWork/DB.java
@@ -2,12 +2,23 @@ package ThreadWork;
 
 /**
  * 实现功能：每次调用add，都将序号自增1
+ *
+ * synchronized隐式锁
+ * {获取锁 }失去锁
  */
 public class DB {
     private int number = 1;
 
     public void add() {
-        System.out.println("序号:" + number);
-        number++;
+        synchronized (DB.class) {//获取锁
+            System.out.println(Thread.currentThread().getName() + "序号:" + number);
+            number++;
+        }//释放锁
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Study Java/API_JAVA/ThreadWork/TestDB.java
+++ Study Java/API_JAVA/ThreadWork/TestDB.java
@@ -6,11 +6,21 @@ public class TestDB {
         Thread t1 = new Thread() {
             @Override
             public void run() {
-                for (int i = 0; i < 11; i++) {
+                for (int i = 0; i < 10; i++) {
                     db.add();
                 }
             }
         };
-      t1.start();
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    db.add();
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+      // 什么时候会出现线程安全问题:什么情况下才会产生并发(多个线程访问了同一个资源而出现的竞争现象)
     }
 }
