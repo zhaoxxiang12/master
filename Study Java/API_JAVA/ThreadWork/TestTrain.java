cf39610970d167f31c2b75f3bd1571f66b388a4a
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 11:54:08 +0800

    update:线程作业(构造方法增加山洞hole类)

--- Study Java/API_JAVA/ThreadWork/TestTrain.java
+++ Study Java/API_JAVA/ThreadWork/TestTrain.java
@@ -2,11 +2,12 @@ package ThreadWork;
 
 public class TestTrain {
     public static void main(String[] args) {
-        Train t1 = new Train("动车D");
-        Train t2 = new Train("高铁G");
-        Train t3 = new Train("直达车Z");
-        Train t4 = new Train("绿皮车");
-        Train t5 = new Train("普快K");
+        Hole hole = new Hole("海底隧道");
+        Train t1 = new Train("动车D",hole);
+        Train t2 = new Train("高铁G",hole);
+        Train t3 = new Train("直达车Z",hole);
+        Train t4 = new Train("绿皮车",hole);
+        Train t5 = new Train("普快K",hole);
 
         t1.start();
         t2.start();
--- Study Java/API_JAVA/ThreadWork/Train.java
+++ Study Java/API_JAVA/ThreadWork/Train.java
@@ -5,18 +5,24 @@ package ThreadWork;
  * 分析：每辆火车就是一个独立的线程
  * 山洞是同一个
  */
-public class Train extends Thread{
+public class Train extends Thread {
+    private Hole hole;
+
     @Override
     public void run() {
-        //火车过山洞的方法
-        System.out.println(getName()+"正在过山洞");
-        try {
-            Thread.sleep(1000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
+        synchronized (hole) {//山洞是唯一的
+            try {
+                //火车过山洞的方法:一辆一辆的过
+                System.out.println(getName() + "正在过山洞");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-    public Train(String name){
+
+    public Train(String name, Hole hole) {
         super(name);
+        this.hole = hole;
     }
 }
