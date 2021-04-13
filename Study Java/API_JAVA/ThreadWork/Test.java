cabc17e6ba820b1c851ff4633929ce627e3aceea
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 10:13:03 +0800

    提交线程学习作业

--- Study Java/API_JAVA/ThreadWork/Test.java
+++ Study Java/API_JAVA/ThreadWork/Test.java
@@ -4,8 +4,12 @@ public class Test {
     public static void main(String[] args) {
         Window window1 = new Window("一号窗口");
         Window window2 = new Window("二号窗口");
+        Window window3 = new Window("三号窗口");
+        Window window4 = new Window("四号窗口");
 
         window1.start();
         window2.start();
+        window3.start();
+        window4.start();
     }
 }
--- Study Java/API_JAVA/ThreadWork/Window.java
+++ Study Java/API_JAVA/ThreadWork/Window.java
@@ -6,6 +6,14 @@ package ThreadWork;
  * 分析：
  * 1.每个窗口就是一个独立的线程
  * 2.100张票就是4个窗口的共享资源
+ *
+ * 重点：synchronized修饰方法(同步方法)
+ * 普通方法：synchronized就表示的是this
+ *
+ * 同步本质：就是让其成为一个不可分割的整体
+ * 静态方法：当前类的字节码对象 类.class
+ * 需要做同步，优先推荐使用同步代码块synchronized(){}
+ * 原则：锁住该锁的，不该锁的不要锁(一旦加锁后，程序虽然安全，但是它的并发降低了)
  */
 public class Window extends Thread {
     private static int number = 100;
@@ -21,12 +29,19 @@ public class Window extends Thread {
     @Override
     public void run() {//窗口需要做的事情
         while (true) {//窗口是一直开着的
-            if (number <= 0) {
-                System.out.println("票已售尽");
-                break;
+            synchronized (Window.class) { //同步加锁防止共享资源产生竞争
+                if (number <= 0) { // 四个线程并发执行  会同时访问共享变量  线程安全 共享资源产生竞争
+                    System.out.println("票已售尽");
+                    break;
+                }
+                System.out.println(getName() + "成功售出" + number + "号票");
+                number--;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            System.out.println(getName() + "成功售出" + number + "号票");
-            number--;
         }
     }
 }
