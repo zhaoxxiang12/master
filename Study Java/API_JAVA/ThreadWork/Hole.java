3cf67151fa50276f06f590b2558f3b25b0c21a94
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 11:33:16 +0800

    add:线程作业(火车过山洞)

---
+++ Study Java/API_JAVA/ThreadWork/Hole.java
@@ -0,0 +1,13 @@
+package ThreadWork;
+
+public class Hole {
+    private String name;
+    public Hole(String name){
+        super();
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
---
+++ Study Java/API_JAVA/ThreadWork/Train.java
@@ -0,0 +1,13 @@
+package ThreadWork;
+
+/**
+ * 5辆火车过山洞,1s过一个
+ * 分析：每辆火车就是一个独立的线程
+ * 山洞是同一个
+ */
+public class Train extends Thread{
+    @Override
+    public void run() {
+        //火车过山洞的方法
+    }
+}
