6d4d88f9ad4211ffa0fce1ee448eecd6125a17ab
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 16:08:19 +0800

    update:线程作业(抢购)

--- Study Java/API_JAVA/ThreadWork/TestShop.java
+++ Study Java/API_JAVA/ThreadWork/TestShop.java
@@ -5,19 +5,19 @@ public class TestShop {
         //1.创建三个线程来模拟三个消费者
         Shopping shopping = new Shopping();
 
-        Thread t1 = new Thread() {
+        Thread t1 = new Thread("赵云") {
             @Override
             public void run() {
                 shopping.sale();
             }
         };
-        Thread t2 = new Thread() {
+        Thread t2 = new Thread("关羽") {
             @Override
             public void run() {
                 shopping.sale();
             }
         };
-        Thread t3 = new Thread() {
+        Thread t3 = new Thread("吕布") {
             @Override
             public void run() {
                 shopping.sale();
