83f8b8d0963b0063bf09d05df38bb42a4f744919
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-04-13 11:15:32 +0800

    update：修改worker3的名字

--- Study Java/API_JAVA/ThreadWork/TestWork.java
+++ Study Java/API_JAVA/ThreadWork/TestWork.java
@@ -4,7 +4,7 @@ public class TestWork {
     public static void main(String[] args) {
         Worker worker1 = new Worker("甲员工");
         Worker worker2 = new Worker("乙员工");
-        Worker worker3 = new Worker("丙员");
+        Worker worker3 = new Worker("丙员工");
 
         worker1.start();
         worker2.start();
