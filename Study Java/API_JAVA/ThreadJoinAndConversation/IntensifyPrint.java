d3a0a62eac0258dae961eeea22fc385d535a49c7
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2021-04-21 23:28:14 +0800

    update:if判断改为while

--- Study Java/API_JAVA/ThreadJoinAndConversation/IntensifyPrint.java
+++ Study Java/API_JAVA/ThreadJoinAndConversation/IntensifyPrint.java
@@ -1,12 +1,16 @@
 package ThreadJoinAndConversation;
 
+/**
+ * 当有两个以上的线程时，需要唤醒所有线程
+ * while 用来唤醒后循环判断flag,只有满足条件才会打印相关内容
+ */
 public class IntensifyPrint {
     private static int flag = 1; //1：打印A 2：欢迎光临
 
 
     public void printA() throws InterruptedException {
         synchronized (this) {
-            if (flag != 1) {
+            while (flag != 1) {
                 this.wait(); //进入线程池(会释放锁)
             }
             System.out.print("A");
@@ -16,13 +20,13 @@ public class IntensifyPrint {
             System.out.println("A");
             flag = 2;
             Thread.sleep(1000);
-            this.notify();// 随机唤醒线程等待池中的任意线程(若线程没有线程，则会空唤醒)
+            this.notifyAll();// 随机唤醒线程等待池中的任意线程(若线程没有线程，则会空唤醒)
         }
     }
 
     public void printB() throws InterruptedException {
         synchronized (this) {
-            if (flag != 2) {
+            while (flag != 2) {
                 this.wait();
             }
             System.out.print("B");
@@ -32,12 +36,12 @@ public class IntensifyPrint {
             System.out.println("B");
             flag = 3;
             Thread.sleep(1000);
-            this.notify();
+            this.notifyAll();
         }
     }
     public void printC() throws InterruptedException {
         synchronized (this) {
-            if (flag != 3) {
+            while (flag != 3) {
                 this.wait();
             }
             System.out.print("C");
@@ -47,7 +51,7 @@ public class IntensifyPrint {
             System.out.println("C");
             flag = 1;
             Thread.sleep(1000);
-            this.notify();
+            this.notifyAll();
         }
     }
 }
