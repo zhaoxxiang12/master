1a1df389ee197a89930009f49e0174b1fda6c38e
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2021-04-23 22:15:43 +0800

    update:线程代码更新

--- Study Java/API_JAVA/ThreadJoinAndConversation/IntensifyPrint.java
+++ Study Java/API_JAVA/ThreadJoinAndConversation/IntensifyPrint.java
@@ -3,6 +3,11 @@ package ThreadJoinAndConversation;
 /**
  * 当有两个以上的线程时，需要唤醒所有线程
  * while 用来唤醒后循环判断flag,只有满足条件才会打印相关内容
+ * synchronized JDK1.8自适应自旋锁
+ * 自旋锁：抢不到就一直去抢，直到抢到锁为止(一直处于忙碌状态，显得非常耗时);如果抢不到就去线程等待休息
+ * 当再次发生唤醒线程信号时，又开始忙碌抢锁，直到抢到锁为止
+ *
+ * 互斥锁：只有两种结果(先抢锁，如果抢不到就不抢了，直接进入线程等待池，等待线程唤醒；如果抢到锁了，则会运行程序)
  */
 public class IntensifyPrint {
     private static int flag = 1; //1：打印A 2：欢迎光临
