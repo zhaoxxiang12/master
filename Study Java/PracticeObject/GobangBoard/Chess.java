8ee2bf587cabfa2c6e41c4ace27b53b9caf3aa86
zhaoxxiang12 <zhaoxiaoxiang666@163.com>
2021-07-11 10:34:40 +0800

    feat:创建棋子容器

---
+++ Study Java/PracticeObject/GobangBoard/Chess.java
@@ -0,0 +1,45 @@
+package GobangBoard;
+
+import javafx.scene.paint.Color;
+
+/**
+ * 棋子描述类
+ */
+public class Chess {
+    private int x;
+    private int y;
+    private Color color;
+    public Chess (){
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Chess(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
