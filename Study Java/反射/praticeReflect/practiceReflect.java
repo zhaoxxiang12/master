983a0671345ed9f977700269139e338567d4e68d
zhaoxiaoxiang12 <zhaoxiaoxiang666@qq.com>
2021-11-19 21:00:03 +0800

    feat:增加反射练习

--- Study Java/反射/praticeReflect/practiceReflect.java
+++ Study Java/反射/praticeReflect/practiceReflect.java
@@ -1,5 +1,25 @@
 package praticeReflect;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 public class practiceReflect {
-    //练习1：已知一个ArrayList,泛型String,里面存储了一些元素，使用反射来实现该集合中存储Integer类型的数据
+    public static void main(String[] args) throws Exception{
+        //练习1：已知一个ArrayList,泛型String,里面存储了一些元素，使用反射来实现该集合中存储Integer类型的数据
+        ArrayList<String> al = new ArrayList<>();
+        al.add("aa");
+        al.add("bb");
+        al.add("cc");
+        //使用反射技术
+        //获取字节码文件对象
+        Class clazz = al.getClass();
+        //获取add方法对象
+        Method addMethod = clazz.getMethod("add", Object.class);
+        //调用add方法对象
+        addMethod.invoke(al,123);
+        addMethod.invoke(al,456);
+        addMethod.invoke(al,789);
+        addMethod.invoke(al,true);
+        System.out.println(al);
+    }
 }
